eb13a06e8f2c13f51552114a7ba4c9a31e28dfb8
Joao Victor Dorl Rossi <joao.vrossi@sptech.school>
2025-05-29 21:28:33 -0300

    Inicio da API do banco de dados para a dashboard

--- web-data-viz/src/models/DashboardModel.js
+++ web-data-viz/src/models/DashboardModel.js
@@ -1,28 +1,17 @@
-var database = require("../database/config")
+var database = require("../database/config");
 
-function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+function buscarCorridasDoUsuario(idUsuario) {
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, genero, dtNasc, email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, genero, dtNasc, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-        INSERT INTO usuario (nome, genero, dtNasc, email, senha) VALUES ('${nome}', '${genero}', '${dtNasc}', '${email}', '${senha}');
+        SELECT idCorrida AS id, nome AS descricao, dtCorrida, distancia, tempo
+        FROM corrida
+        WHERE fkUsuario = ${idUsuario}
+        ORDER BY dtCorrida DESC
+        LIMIT 10;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    autenticar,
-    cadastrar
+    buscarCorridasDoUsuario
 };
--- web-data-viz/src/routes/Dashboard.js
+++ web-data-viz/src/routes/Dashboard.js
@@ -1,15 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var corridaController = require("../controllers/corridaController");
 
-var usuarioController = require("../controllers/usuarioController");
-
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
-router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
-})
-
-router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
-});
+router.get("/usuario/:idUsuario", corridaController.listarCorridasUsuario);
 
 module.exports = router;
